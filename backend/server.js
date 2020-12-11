9f36d8ffd81701fc104f93f1ead6e1abfe2f5f36
Jesstradiol <71665951+Jesstradiol@users.noreply.github.com>
2020-12-11 12:07:58 -0600

    new

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import data from './data.js';
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
+app.get('/api/albums', (req, res) => {
   res.send(data.albums);
 });
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serving Lewks at http://localhost:${port}`);
