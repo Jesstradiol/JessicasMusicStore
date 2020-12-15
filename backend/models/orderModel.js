67ecb829cb1ec720da85fe8cd1938c281e2d8ab2
Jesstradiol <71665951+Jesstradiol@users.noreply.github.com>
2020-12-15 12:57:46 -0600

    Improved css

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,10 +4,10 @@ const orderSchema = new mongoose.Schema(
   {
     orderItems: [
       {
-        name: { type: String, required: true },
+        AlbumTitle: { type: String, required: true },
         qty: { type: Number, required: true },
         image: { type: String, required: true },
-        price: { type: Number, required: true },
+        AlbumCost: { type: Number, required: true },
         album: {
           type: mongoose.Schema.Types.ObjectId,
           ref: 'Album',
@@ -33,7 +33,9 @@ const orderSchema = new mongoose.Schema(
     shippingPrice: { type: Number, required: true },
     taxPrice: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId,
+       ref: 'User',
+        required: true },
     isPaid: { type: Boolean, default: false },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -82,4 +82,5 @@ orderRouter.put(
   })
 );
 
+
 export default orderRouter;
